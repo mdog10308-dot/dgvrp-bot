24b6bb3c556597b20918a6cfaa4eac16a10296ee
mdog10308-dot <mdog10308@gmail.com>
2026-04-09 19:07:46 +0100

    Update dgvrp_bot.py

--- dgvrp_bot.py
+++ dgvrp_bot.py
@@ -1,8 +1,25 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    # Render uses the 'PORT' variable automatically
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 import discord
 from discord.ext import commands
 from discord import app_commands
 import json
-import os
 from dotenv import load_dotenv
 from datetime import datetime
 
